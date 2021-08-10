5c565b70ee224a1d7258973ff75e59d3d274a78f
Vinícius Vargas <santunionivinicius@gmail.com>
2021-08-10 09:42:00 -0300

    chore: add type hints and upgrade map_keys algorithm

--- src/communication.py
+++ src/communication.py
@@ -1,14 +1,15 @@
 import logging
+from asyncio import Queue
 
-from aiokafka import AIOKafkaProducer
+from aiokafka import AIOKafkaProducer, AIOKafkaConsumer, ConsumerRecord
 from pydantic import ValidationError
 
-from src.models.mat_events import ServiceLetter
+from src.models.mat_events import ServiceLetter, ServiceResponse
 
 logger = logging.getLogger(__name__)
 
 
-async def kafka_produce(topic: str, response_producer: AIOKafkaProducer, responses):
+async def kafka_produce(topic: str, response_producer: AIOKafkaProducer, responses: Queue[ServiceResponse]):
     while True:
         service_response = await responses.get()
         await response_producer.send(topic=topic, value=service_response)
@@ -16,8 +17,8 @@ async def kafka_produce(topic: str, response_producer: AIOKafkaProducer, respons
         logger.info("Sent: %s", service_response.event_trace)
 
 
-async def kafka_consume(letter_consumer, letters):
-    async for msg in letter_consumer:
+async def kafka_consume(letter_consumer: AIOKafkaConsumer, letters: Queue[ServiceLetter]):
+    async for msg in letter_consumer:  # type: ConsumerRecord
         logger.debug(
             "Received message from Kafka: topic=%s, partition=%s, offset=%s, key=%s, value=%s, timestamp=%s",
             msg.topic, msg.partition, msg.offset, msg.key, msg.value, msg.timestamp
