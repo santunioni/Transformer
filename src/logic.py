4f6586fb3311e0d45a5e2548ae9958a96d2ac90e
Vinícius Vargas <santunionivinicius@gmail.com>
2021-08-11 16:23:04 -0300

    chore: remove warnings for unmapped_data

--- src/logic.py
+++ src/logic.py
@@ -16,8 +16,10 @@ async def process_message(letters: Queue[ServiceLetter], responses: Queue[Servic
             mapping=service_letter.config.mapping,
             preserve_unmapped=service_letter.config.preserve_unmapped
         )
-        if unmapped_data != {}:
+
+        if not service_letter.config.preserve_unmapped and unmapped_data != {}:
             logger.warning("Unmapped data for event_trace=%s: %s", service_letter.event_trace, str(unmapped_data))
+
         service_response.data = mapped_data
         await responses.put(service_response)
         letters.task_done()
