c7b7fb1509c52ab23b7ec5758e0893bb7d3ac0c9
otavio <otavio-dantas@live.com>
2021-08-11 11:39:00 -0300

    chore: exclude service_identifier

--- src/settings.py
+++ src/settings.py
@@ -13,7 +13,6 @@ class EnvironmentSettings(BaseSettings):
     KAFKA_TOPIC_FIELD_TRANSLATOR: str
     KAFKA_TOPIC_SERVICE_RESPONSE: str
     KAFKA_CONSUMER_GROUP_ID: str = "field_translator"
-    SERVICE_IDENTIFIER: str = "field_translator"
 
     NUMBER_OF_MAX_CONCURRENT_MESSAGES: int = 0
     NUMBER_OF_CONCURRENT_SERVICE_CALLS: int = 1000
