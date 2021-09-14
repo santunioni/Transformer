8c2a0b1445ad65034ef284fc69336ef668dffa14
Vinícius Vargas <santunionivinicius@gmail.com>
2021-09-14 11:25:08 -0300

    update dependencies

--- tests/measure.py
+++ tests/measure.py
@@ -1,4 +1,5 @@
 import asyncio
+import contextlib
 import logging
 import uuid
 
@@ -10,6 +11,14 @@ from src.the_flash.feeders.consumer_feeders.aiokafka.factory import kafka_factor
 from tests.factory.letter_factory import data_factory, config_factory
 
 logger = logging.getLogger(__name__)
+from aiocache import cached
+
+
+@cached()
+async def get_producer():
+    _, producer = kafka_factory(KafkaSettings())
+    await contextlib.AsyncExitStack().enter_async_context(producer)
+    return producer
 
 
 def letter_gen(number: int):
@@ -20,23 +29,32 @@ def letter_gen(number: int):
             data=data_factory(),
             config=config_factory(),
             index_in_flow=0
+        ).json().encode("utf-8")
+
+
+async def send_sequence(number: int):
+    producer = await get_producer()
+    for letter in letter_gen(number):
+        await producer.send_and_wait(
+            topic=KafkaSettings().KAFKA_TOPIC_FIELD_TRANSLATOR,
+            value=letter
         )
 
 
-async def main(number_of_messages: int):
-    _, producer = kafka_factory(KafkaSettings())
-    async with producer:
-        for letter in letter_gen(number_of_messages):
-            await producer.send_and_wait(
-                topic=KafkaSettings().KAFKA_TOPIC_FIELD_TRANSLATOR,
-                value=letter.json().encode("utf-8")
-            )
+async def send_parallel(number: int):
+    producer = await get_producer()
+    await asyncio.gather(*[
+        producer.send_and_wait(
+            topic=KafkaSettings().KAFKA_TOPIC_FIELD_TRANSLATOR,
+            value=letter
+        ) for letter in letter_gen(number)
+    ])
 
 
 if __name__ == "__main__":
     load_dotenv(find_dotenv(filename="local.env", raise_error_if_not_found=True))
     try:
-        asyncio.run(main(1000))
+        asyncio.run(send_sequence(1000))
     except BaseException as e:
         print(e)
         print("Shutting down.")
