71c8b0baa78ab514ddea0d828c23327bbb6e6a8c
Vinícius Vargas <santunionivinicius@gmail.com>
2021-10-26 14:50:44 -0300

    fix: pipedrive and dinamize unpack commands now create nested dict

--- transformer/transformers/dinamize_unpack.py
+++ transformer/transformers/dinamize_unpack.py
@@ -1,17 +1,19 @@
-from typing import Dict, Tuple
+from typing import Any, Dict, Optional, Tuple
 
 from transformer.transformers.abstract import ExtraHashableModel, Transformer
 
 
 class DinamizeUnpackConfig(ExtraHashableModel):
-    ...
+    __root__: Optional[Any]
 
 
 class DinamizeUnpack(Transformer[DinamizeUnpackConfig]):
     def transform(self, data: Dict, metadata: Dict) -> Tuple[Dict, Dict]:
         dt = {
-            field.get("Name"): field.get("Value")
-            for field in data.get("custom_fields", {}).values()
+            "dinamize": {
+                field.get("Name"): field.get("Value")
+                for field in data.get("custom_fields", {}).values()
+            }
         }
-        dt.update(data.get("fixed_fields", {}))
+        dt["dinamize"].update(data.get("fixed_fields", {}))
         return dt, metadata
--- transformer/transformers/pipedrive_unpack.py
+++ transformer/transformers/pipedrive_unpack.py
@@ -1,23 +1,28 @@
-from typing import Dict, Tuple
+from typing import Any, Dict, Optional, Tuple
 
 from transformer.transformers.abstract import ExtraHashableModel, Transformer
 
 
 class PipedriveUnpackConfig(ExtraHashableModel):
-    ...
+    __root__: Optional[Any]
 
 
 class PipedriveUnpack(Transformer[PipedriveUnpackConfig]):
     def transform(self, data: Dict, metadata: Dict) -> Tuple[Dict, Dict]:
-        metadata.update(
-            {
-                k: v
-                for k, v in data.get("meta", {}).items()
-                if k
-                in ("action", "change_source", "company_id", "host", "id", "object")
+        _object = data.get("meta", {}).get("object")
+        r_data = {
+            "pipedrive": {
+                f"{_object}": {
+                    key: value for key, value in data.get("current", {}).items()
+                },
+                "company": {"id": data.get("meta", {}).get("company_id")},
             }
-        )
-        metadata.update(
-            {"origin": "pipedrive", "type": data.get("meta", {}).get("object")}
-        )
-        return data.get("current", {}), metadata
+        }
+        r_metadata = {
+            key: value
+            for key, value in data.get("meta", {}).items()
+            if key in ("action", "change_source", "company_id", "host", "id")
+        }
+        r_metadata.update({"origin": "pipedrive", "object": _object})
+        r_metadata.update(metadata)
+        return r_data, r_metadata
