f7abc7763439fb4f84368faa4fcc9616df99b88d
Vinícius Vargas <vinicius.svargas@luizalabs.com>
2022-02-21 18:56:44 -0300

    chore: remove service specific transformations

--- transformer/registry.py
+++ transformer/registry.py
@@ -8,16 +8,8 @@ from transformer.transformers.aggregate_keys import (
     AggregateKeyValueConfig,
 )
 from transformer.transformers.del_key import DeleteKeys, DeleteKeysConfig
-from transformer.transformers.dinamize_unpack import (
-    DinamizeUnpack,
-    DinamizeUnpackConfig,
-)
 from transformer.transformers.flatters import Flatter, FlatterConfigPydantic, Unflatter
 from transformer.transformers.map_keys import MapKeys, MapKeysConfig
-from transformer.transformers.pipedrive_unpack import (
-    PipedriveUnpack,
-    PipedriveUnpackConfig,
-)
 from transformer.transformers.value_sanitizer import (
     ValueSanitizer,
     ValueSanitizerConfig,
@@ -31,8 +23,6 @@ CommandNames = Literal[
     "delete-keys",
     "aggregate-keys",
     "add-key-values",
-    "pipedrive-unpack",
-    "dinamize-unpack",
     "flatten",
     "unflatten",
 ]
@@ -46,8 +36,6 @@ NAME_TO_CONFIG_AND_TRANSFORM_CLASSES: Mapping[
     "delete-keys": (DeleteKeysConfig, DeleteKeys),
     "aggregate-keys": (AggregateKeyValueConfig, AggregateKeyValue),
     "add-key-values": (AddKeyValuesConfig, AddKeyValues),
-    "pipedrive-unpack": (PipedriveUnpackConfig, PipedriveUnpack),
-    "dinamize-unpack": (DinamizeUnpackConfig, DinamizeUnpack),
 }
 
 
